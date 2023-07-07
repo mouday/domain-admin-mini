9db7653c383e591678c9c416471def593e629839
pengshiyu <1940607002@qq.com>
2023-07-07 23:05:36 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+
   plugins: [
     uni(),
     Components({
