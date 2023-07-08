f63b9cb8819c34ad6269c5f723908074f8e8f5dd
pengshiyu <1940607002@qq.com>
2023-07-08 11:02:06 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import uni from '@dcloudio/vite-plugin-uni'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 
+
+// ref: Vite 部署后 _plugin-vue_export-helper 文件 404 问题
+// https://juejin.cn/post/7202099191681712188
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: './',
 
   plugins: [
     uni(),
@@ -13,4 +21,22 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+          )
+        },
+      },
+    },
+  },
 })
